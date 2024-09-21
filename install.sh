8b41104a9431465d33204cbec51bf043e7bafe71
anaxios <skybosh.xyz@outlook.com>
2024-09-21 11:01:52 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,32 +4,32 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Update the package index
-sudo apt-get update
+apt-get update
 
 # Install dependencies
-sudo apt-get install ca-certificates curl
+apt-get install ca-certificates curl
 
-sudo install -m 0755 -d /etc/apt/keyrings
+install -m 0755 -d /etc/apt/keyrings
 
 # Add Docker GPG key
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add Docker repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update the package index again
-sudo apt update
+apt update
 
 # Install Docker
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Start and enable Docker service
-sudo systemctl start docker
-sudo systemctl enable docker
+systemctl start docker
+systemctl enable docker
 
 # Verify installation
-sudo docker run hello-world
+docker run hello-world
